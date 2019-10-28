e2bc9639e27b8ca0f18193d4175e0f86e67a489c
JahazielGuzman <jahaziel.guzman93@gmail.com>
2019-10-28 17:28:20 -0400

    heroku app is backend

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-const BASE_URL = "https://e-bae.herokuapp.com/";
-const ITEMS_URL = `${BASE_URL}items`;
+const BASE_URL = "https://e-bae.herokuapp.com";
+const ITEMS_URL = `${BASE_URL}/items`;
 const overlayDiv = document.getElementById('overlay')
 const specialBoxDiv = document.getElementById('specialBox')
 const body = document.getElementById('body')
